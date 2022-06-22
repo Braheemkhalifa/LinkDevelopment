d1e4881857ea29b347bb34789979c272609816ca
Braheemkhalifa <braheem.khalifa@gmail.com>
2022-06-22 12:00:43 +0200

    d

--- src/components/Icons/ArrowDownIcon.tsx
+++ src/components/Icons/ArrowDownIcon.tsx
@@ -1,13 +1,6 @@
 const ArrowDownIcon = () => {
   return (
-    <svg
-      version="1.1"
-      id="Layer_1"
-      xmlns="http://www.w3.org/2000/svg"
-      x="0px"
-      y="0px"
-      viewBox="0 0 330 330"
-    >
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 330 330">
       <path
         id="XMLID_225_"
         d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,14 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../styles/globals.scss';
 import Layout from 'components/Shared/Layout';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Link Development App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
